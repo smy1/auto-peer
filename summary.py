8ad32d24bf727e537ea781779f4617046b3c69c5
Sia Ming Yean <46472108+smy1@users.noreply.github.com>
2025-06-20 15:01:39 +0800

    Add files via upload

--- summary.py
+++ summary.py
@@ -1,3 +1,9 @@
+'''
+This script looks for a list of csv files that contain video coding scores by AI,
+then compiles the scores such as each row is a file and each column is a coding criterion.
+by MY Sia
+June 2025
+'''
 import glob
 from pathlib import Path
 import pandas as pd
@@ -18,7 +24,7 @@ df['wh']=[]
 df['distancing']=[]
 
 ##locate csv files
-proj = "C:/Users/user/Desktop/smy/PROJECTS/DREI"
+proj = "C:/Users/user/Desktop/smy/PROJECTS/DREI/3_compile_scores"
 files_path = Path(f"{proj}")
 file_list = glob.glob(f"{files_path}/*.csv")
 
@@ -27,14 +33,15 @@ for csv_file in file_list:
     csv=pd.read_csv(csv_file)
     kid=csv_file[len(proj)+1:len(proj)+4] ##ID of child
     model=csv_file[len(proj)+14:-4] ##AI model
-    ##subset data
+    ####subset all DR data
     justdr=csv[['Evaluate', 'Expand', 'Repeat', 'Completion', 'Recall', 'Open-end', 'Wh', 'Distancing']]
     sumscore=justdr.sum() ##sum every column
     # Prompt=sum(sumscore[3:8]) ##prompt score is the total of crowd
     # PEER=sum(sumscore)
-    Prompt=sum(sumscore[4:8]) ##ignore completion
-    PEER=sum(sumscore[0:2, 4:8])
-    # header=list(justdr.columns) ##get names of columns
+    ####subset data without completion
+    newdr=sumscore[['Evaluate', 'Expand', 'Repeat', 'Recall', 'Open-end', 'Wh', 'Distancing']]
+    Prompt=sum(newdr[3:8])
+    PEER=sum(newdr)
     ##aggregate the data to the emptyframe
     newrow=pd.DataFrame({'ID': [kid], 'model': [model], 'PEER': [PEER],
                     'prompt': [Prompt], 'evaluate': [sumscore['Evaluate']], 'expand': [sumscore['Expand']], 'repeat': [sumscore['Repeat']],
